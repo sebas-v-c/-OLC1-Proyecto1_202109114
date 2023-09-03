56417f66aa2a50bd5fb5e096b310c07f4aca58f0
Sebastian Vasquez <sebas.vasquez010@gmail.com>
2023-09-03 11:28:47 -0600

    test function name changed

--- StatPyConvertor/src/test/java/gt/edu/usac/ingenieria/analyzer/ParserSTPTest.java
+++ StatPyConvertor/src/test/java/gt/edu/usac/ingenieria/analyzer/ParserSTPTest.java
@@ -1,4 +1,4 @@
-package gt.edu.usac.ingenieria;
+package gt.edu.usac.ingenieria.analyzer;
 
 import gt.edu.usac.ingenieria.analyzer.statpy.Parser;
 import gt.edu.usac.ingenieria.analyzer.statpy.StatPyLexer;
@@ -10,10 +10,10 @@ import java.io.StringReader;
 public class ParserSTPTest {
 
     @Test
-    public void parserAnalysis() throws Exception{
+    public void testInputFile() throws Exception{
         String expression = "2*4+(6*3)$adfalksd; lfasn ";
         StatPyLexer lexer = new StatPyLexer(new StringReader(expression));
         Parser p = new Parser(lexer);
-        p.error
     }
+
 }
