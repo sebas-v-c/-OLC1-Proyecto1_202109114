2f8fcbf4483bfc8bae72b1acfe502f23ed5a60ee
Sebastian Vasquez <sebas.vasquez010@gmail.com>
2023-09-03 15:02:20 -0600

    UI theme

--- StatPyConvertor/src/main/java/gt/edu/usac/ingenieria/App.java
+++ StatPyConvertor/src/main/java/gt/edu/usac/ingenieria/App.java
@@ -1,5 +1,10 @@
 package gt.edu.usac.ingenieria;
 
+import com.formdev.flatlaf.FlatDarculaLaf;
+import com.formdev.flatlaf.FlatDarkLaf;
+import com.formdev.flatlaf.FlatIntelliJLaf;
+import com.formdev.flatlaf.FlatLightLaf;
+import com.formdev.flatlaf.themes.FlatMacDarkLaf;
 import gt.edu.usac.ingenieria.editor.EditorController;
 import gt.edu.usac.ingenieria.editor.EditorView;
 
@@ -8,6 +13,8 @@ import java.util.Locale;
 public class App {
     public static void main(String[] args) {
         Locale.setDefault(new Locale("es"));
+
+        FlatIntelliJLaf.setup();
 
         EditorView editor = new EditorView();
         EditorController controller = new EditorController(editor);
