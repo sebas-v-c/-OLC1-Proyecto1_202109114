34cb11e7ed9d2689e4709f37170c4140b3c75127
Sebastian Vasquez <sebas.vasquez010@gmail.com>
2023-09-04 10:43:28 -0600

    TODOS

--- StatPyConvertor/src/main/java/gt/edu/usac/ingenieria/editor/EditorController.java
+++ StatPyConvertor/src/main/java/gt/edu/usac/ingenieria/editor/EditorController.java
@@ -2,6 +2,12 @@ package gt.edu.usac.ingenieria.editor;
 
 public class EditorController {
     EditorView view;
+    // TODO Open file
+    // TODO save file
+    // TODO save as file
+    // TODO Change analyzer
+    // TODO create execute method
+    // the execute either traduce or analyze only
     public EditorController(EditorView view) {
         this.view = view;
     }
