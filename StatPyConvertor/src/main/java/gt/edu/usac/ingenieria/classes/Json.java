85794976a8d2b04b240221e34fc603ef8b990712
Sebastian Vasquez <sebas.vasquez010@gmail.com>
2023-09-07 10:42:24 -0600

    Json object finished

--- StatPyConvertor/src/main/java/gt/edu/usac/ingenieria/classes/Json.java
+++ StatPyConvertor/src/main/java/gt/edu/usac/ingenieria/classes/Json.java
@@ -1,4 +1,28 @@
 package gt.edu.usac.ingenieria.classes;
 
+import gt.edu.usac.ingenieria.lang.json.KeyVal;
+
+import java.util.HashMap;
+
 public class Json {
+    private final HashMap<String, Object> content;
+
+    public Json(){
+        content = new HashMap<>();
+    }
+
+    public Json(String key, Object value){
+        content = new HashMap<>();
+        content.put(key, value);
+    }
+
+    public void addKeyValue(String key, Object object){
+        content.put(key, object);
+    }
+
+    public Object getValue(String key){
+        return content.get(key);
+    }
+
+
 }
