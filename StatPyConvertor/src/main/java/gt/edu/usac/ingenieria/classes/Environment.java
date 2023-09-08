c2d95e6e15c571fb050a5c99ed00a6de337c4c4d
Sebastian Vasquez <sebas.vasquez010@gmail.com>
2023-09-07 23:33:55 -0600

    implemented multiple pie and bars graphs

--- StatPyConvertor/src/main/java/gt/edu/usac/ingenieria/classes/Environment.java
+++ StatPyConvertor/src/main/java/gt/edu/usac/ingenieria/classes/Environment.java
@@ -2,34 +2,33 @@ package gt.edu.usac.ingenieria.classes;
 
 import gt.edu.usac.ingenieria.lang.statpy.expression.Value;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Environment {
     private HashMap<String, Object> globals;
-    private HashMap<String, Object> pie;
-    private HashMap<String, Object> bars;
+    private ArrayList<HashMap<String, Object>> pie = new ArrayList<>();
+    private ArrayList<HashMap<String, Object>> bars = new ArrayList<>();
 
     public Environment(){
         globals = new HashMap<>();
-        pie = new HashMap<>();
-        bars = new HashMap<>();
     }
 
     public void clearEnv(){
         globals = new HashMap<>();
-        pie = new HashMap<>();
-        bars = new HashMap<>();
+        pie.clear();
+        bars.clear();
     }
     public void updateVar(String id, Object val) throws IDNotFoundException {
         Object glob = getGlobalsValue(id);
         if (glob != null) {
             updateGlobalsValue(id, val);
         }
-        Object pie = getPieValue(id);
+        Object pie = getPieValue(id, this.pie.size()-1);
         if (pie != null){
             updatePieValue(id, val);
         }
-        Object bars = getBarsValue(id);
+        Object bars = getBarsValue(id, this.bars.size()-1);
         if (bars != null){
             updateBarsValue(id, val);
         }
@@ -49,11 +48,11 @@ public class Environment {
         if (glob != null) {
             return glob;
         }
-        Object pie = ((Value)getPieValue(id)).value();
+        Object pie = ((Value)getPieValue(id, this.pie.size()-1)).value();
         if (pie != null){
             return pie;
         }
-        Object bars = ((Value)getBarsValue(id)).value();
+        Object bars = ((Value)getBarsValue(id, this.pie.size()-1)).value();
         if (bars != null){
             return bars;
         }
@@ -69,19 +68,38 @@ public class Environment {
         globals.put(id, val);
     }
 
-    public Object getPieValue(String id){
-        return pie.get(id);
+    public Object getPieValue(String id, int pos){
+        return pie.get(pos).get(id);
     }
 
     public void updatePieValue(String id, Object val){
-        pie.put(id, val);
+        updateGraphs(id, val, pie);
     }
 
-    public Object getBarsValue(String id){
-        return bars.get(id);
+    private void updateGraphs(String id, Object val, ArrayList<HashMap<String, Object>> graph) {
+        if (pie.isEmpty()){
+            pie.add(new HashMap<>());
+            pie.get(0).put(id, val);
+            return;
+        }
+        HashMap<String, Object> current = pie.get(pie.size()-1);
+        current.put(id, val);
+    }
+
+    public void addNewPie(){
+        pie.add(new HashMap<>());
+    }
+
+    public Object getBarsValue(String id, int pos){
+
+        return bars.get(pos).get(id);
     }
 
     public void updateBarsValue(String id, Object val){
-        bars.put(id, val);
+        updateGraphs(id, val, bars);
+    }
+
+    public void addNewBars(){
+        bars.add(new HashMap<>());
     }
 }
